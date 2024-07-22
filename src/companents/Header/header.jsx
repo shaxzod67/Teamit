3ecda2790685d8e3cc40afa61fb4c8041f23862a
shaxzod67 <uzbuz741@gmail.com>
2024-07-22 13:27:56 +0500

    new

--- src/companents/Header/header.jsx
+++ src/companents/Header/header.jsx
@@ -1,30 +1,27 @@
 // import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 // import { Swiper, SwiperSlide } from 'swiper/react';
-import React, { useRef, useState } from 'react';
-import { Select } from 'antd';
+import React, { useRef, useState } from "react";
+import { Select } from "antd";
 import { FaPhone } from "react-icons/fa6";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 
-
-import { Carousel } from 'antd';
+import { Carousel } from "antd";
 const contentStyle = {
-  height: '360px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
+  height: "360px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
 };
 export const Header = () => {
-
   const [kurs, setKurs] = useState(false);
   const clickKurs = () => {
     setKurs(!kurs);
-  }
-
+  };
 
   const handleTel = () => {
     window.location.href = "tel:+998995507799";
@@ -32,7 +29,7 @@ export const Header = () => {
   return (
     <div>
       <header>
-        <nav >
+        <nav>
           <div className="nav_in">
             <div className="logoDiv">
               <img src="https://www.teamit.uz/teamitlogo.png" alt="Teamit" />
@@ -43,7 +40,6 @@ export const Header = () => {
                   <option value="2">Grafik dizayn</option>
                   <option value="3">3D max</option>
                 </select> */}
-
 
                 {/*
                 {
@@ -71,32 +67,20 @@ export const Header = () => {
                 }
                 */}
 
-
-                <div className='select_box' >
+                <div className="select_box">
                   <button onClick={clickKurs}>
                     Kurslar
-                    {
-                      kurs
-                        ? <IoIosArrowUp />
-                        : <IoIosArrowDown />
-
-                    }
+                    {kurs ? <IoIosArrowUp /> : <IoIosArrowDown />}
                   </button>
                 </div>
               </div>
             </div>
 
-
-            <div >
+            <div>
               <button onClick={handleTel} className="telfon">
                 <FaPhone />
-                <p className="tel1">
-                  +998 (99) 550 77 99
-                </p>
-                <p className="tel2">
-                  Call
-                </p>
-
+                <p className="tel1">+998 (99) 550 77 99</p>
+                <p className="tel2">Call</p>
               </button>
             </div>
             {/* <div className="lineNavbar">
@@ -107,98 +91,103 @@ export const Header = () => {
           </div>
         </nav>
 
-        {
-          kurs
-          && <div className='w-[100%] fixed bg-[#f2f2f2] z-[8]'>
+        {kurs && (
+          <div className="w-[100%] fixed bg-[#f2f2f2] z-[8]">
             <div className="kurs_box">
               <div className="kurs_box1">
-                <img src="https://www.teamit.uz/courses/web.jpg" alt="Web dasturlash" />
-                <h2 >
-                  Web dasturlash
-                </h2>
+                <img
+                  src="https://www.teamit.uz/courses/web.jpg"
+                  alt="Web dasturlash"
+                />
+                <h2>Web dasturlash</h2>
               </div>
 
               <div className="kurs_box1">
-                <img src="https://www.teamit.uz/courses/graphic.jpg" alt="grafik dizayn" />
-                <h2 >
-                  Grafik dizayn
-                </h2>
+                <img
+                  src="https://www.teamit.uz/courses/graphic.jpg"
+                  alt="grafik dizayn"
+                />
+                <h2>Grafik dizayn</h2>
               </div>
               <div className="kurs_box1">
-                <img src="https://www.teamit.uz/courses/3ds.jpg" alt=" 3D max" />
-                <h2 >
-                  3D max
-                </h2>
+                <img
+                  src="https://www.teamit.uz/courses/3ds.jpg"
+                  alt=" 3D max"
+                />
+                <h2>3D max</h2>
               </div>
 
               <div className="kurs_box1">
-                <img src="https://www.teamit.uz/courses/auto.jpg" alt=" auto cad" />
-                <h2 >
-                  Auto CAD
-                </h2>
+                <img
+                  src="https://www.teamit.uz/courses/auto.jpg"
+                  alt=" auto cad"
+                />
+                <h2>Auto CAD</h2>
               </div>
-
-
 
               <div className="kurs_box1">
-                <img src="https://www.teamit.uz/courses/ofis.jpg" alt=" ofis dasturlari" />
-                <h2 >
-                  Ofis dasturlari
-                </h2>
+                <img
+                  src="https://www.teamit.uz/courses/ofis.jpg"
+                  alt=" ofis dasturlari"
+                />
+                <h2>Ofis dasturlari</h2>
               </div>
               <div className="kurs_box1">
-                <img src="https://www.teamit.uz/courses/roboto.jpg" alt="Robototexnika" />
-                <h2 >
-                 Robototexnika
-                </h2>
+                <img
+                  src="https://www.teamit.uz/courses/roboto.jpg"
+                  alt="Robototexnika"
+                />
+                <h2>Robototexnika</h2>
               </div>
-
             </div>
           </div>
-        }
+        )}
 
         <div className="header_box">
           <div className="header_chap">
-            <h2>
-              Kelajak kasblarni biz bilan o'rganing
-            </h2>
-            <button>
-              Ro'yxatdan otish
-            </button>
+            <h2>Kelajak kasblarni biz bilan o'rganing</h2>
+            <button>Ro'yxatdan otish</button>
           </div>
           <div className="header_ong">
-
-
-
-
-
-            <Carousel autoplay>
+            <Carousel autoplay className="swiper">
               <div>
-                <h3 style={contentStyle}><img src="https://www.teamit.uz/courses/web.jpg" alt="Web dasturlash" /></h3>
+                <h3 style={contentStyle}>
+                  <img
+                    src="https://www.teamit.uz/courses/web.jpg"
+                    alt="Web dasturlash"
+                  />
+                </h3>
               </div>
               <div>
-                <h3 style={contentStyle}><img src="https://www.teamit.uz/courses/graphic.jpg" alt="grafik dizayn" /></h3>
+                <h3 style={contentStyle}>
+                  <img
+                    src="https://www.teamit.uz/courses/graphic.jpg"
+                    alt="grafik dizayn"
+                  />
+                </h3>
               </div>
               <div>
-                <h3 style={contentStyle}><img src="https://www.teamit.uz/courses/3ds.jpg" alt="" /></h3>
+                <h3 style={contentStyle}>
+                  <img src="https://www.teamit.uz/courses/3ds.jpg" alt="" />
+                </h3>
               </div>
               <div>
-                <h3 style={contentStyle}><img src="https://www.teamit.uz/courses/auto.jpg" alt="" /></h3>
+                <h3 style={contentStyle}>
+                  <img src="https://www.teamit.uz/courses/auto.jpg" alt="" />
+                </h3>
               </div>
               <div>
-                <h3 style={contentStyle}><img src="https://www.teamit.uz/courses/ofis.jpg" alt="" /></h3>
+                <h3 style={contentStyle}>
+                  <img src="https://www.teamit.uz/courses/ofis.jpg" alt="" />
+                </h3>
               </div>
               <div>
-                <h3 style={contentStyle}> <img src="https://www.teamit.uz/courses/roboto.jpg" alt="" /></h3>
+                <h3 style={contentStyle}>
+                  {" "}
+                  <img src="https://www.teamit.uz/courses/roboto.jpg" alt="" />
+                </h3>
               </div>
-
-
             </Carousel>
-
-
-
-
-
 
             {/* <Swiper
               spaceBetween={30}
@@ -225,13 +214,8 @@ export const Header = () => {
         </div>
       </header>
     </div>
-  )
-}
-
-
-
-
-
+  );
+};
 
 // export default function App() {
 //   return (
