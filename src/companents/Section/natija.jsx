12cfc8a9fe036170c0e354977da9e6d7652aed25
shaxzod67 <uzbuz741@gmail.com>
2024-08-07 11:48:00 +0500

    natijalar

--- src/companents/Section/natija.jsx
+++ src/companents/Section/natija.jsx
@@ -1,46 +1,29 @@
-import { Col, Statistic } from "antd";
-import Aos from "aos";
-import { useEffect } from "react";
-import CountUp from "react-countup";
-
-const formatter = (value) => <CountUp end={value} separator="" />;
-
 function Natija() {
-  useEffect(() => {
-    Aos.init({ duration: 2000 });
-  }, []);
   return (
     <div className="natija">
-      <h2>Natijalarimiz</h2>
-      <div className="natija_box">
-        <div className="natija_box1">
-          <Col span={12}>
-            <Statistic
-              title="Bitiruvchilar"
-              value={1700}
-              formatter={formatter}
-            />
-          </Col>
-        </div>
-        <div className="natija_box2">
-          <p>
-            Ishga kirish<br />
-            40% - 50% 
-          </p>
-        </div>
-        <div className="natija_box3">
-          <h3>Maoshalari</h3>
-          {/* <Col span={12}>
-            <Statistic title="Dasturlash" value={200}  formatter={formatter} />
-            <Statistic title="Grafik dizayn" value={350} formatter={formatter} />
-            <Statistic title="Arxitektura" value={300} formatter={formatter} />
-          </Col> */}
-
-          <p>
-            Dasturlash(junior) - 300$ <br />
-            Grafik dizayn - 350$ <br />
-            Arxitektura - 250$ - 300$
-          </p>
+      <div className="natija_info">
+        <h3>Natijalarimiz</h3>
+        <div className="natija_boxs">
+          <div className="natija_box">
+            <div>
+              <h2>Bitiruvchilar</h2>
+              <p>400 o'quvchi</p>
+            </div>
+          </div>
+          <div className="natija_box">
+            <div>
+              <h2>Ishga kirish</h2>
+              <p>40-50%</p>
+            </div>
+          </div>
+          <div className="natija_box">
+            <div>
+              <h2>Maoshlar (junior)</h2>
+              <p>Dasturchilar - 200$</p>
+              <p>Grafik dizayn- 300$</p>
+              <p>Arxitektura - 250$</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
