524b9339a857f71b40b1c402645fee4ecd5960c8
shaxzod67 <uzbuz741@gmail.com>
2024-08-10 15:07:49 +0500

    eerr

--- src/companents/Section/team.jsx
+++ src/companents/Section/team.jsx
@@ -5,6 +5,7 @@ import { FaGithub } from "react-icons/fa6";
 function Team() {
   const team = [
     {
+      id:1,
       image:
         "https://avatars.mds.yandex.net/i?id=04c92f2d7947f2f3460e996c8ee063609d02026a-5232865-images-thumbs&n=13",
       name: "Yormuhammad",
@@ -19,16 +20,17 @@ function Team() {
     {
       image:
         "https://avatars.mds.yandex.net/i?id=04c92f2d7947f2f3460e996c8ee063609d02026a-5232865-images-thumbs&n=13",
-      name: "Yormuhammad",
-      surname: "G'aniyev",
-      position: "Direktor",
+      name: "Iskandar",
+      surname: "Oqilov",
+      position: "Grafik dizayn",
       university: "Toshkent Axborot Texnologiyalar Universiteti",
-      experience: "4-yil",
+      experience: "2-yil",
       telegram: "https://t.me/olimjonov_44",
       instagram: "https://www.instagram.com/olimjonov.23",
       gitHub: "https://github.com/shaxzod67",
     },
     {
+      id:2,
       image:
         "https://avatars.mds.yandex.net/i?id=04c92f2d7947f2f3460e996c8ee063609d02026a-5232865-images-thumbs&n=13",
       name: "Yormuhammad",
@@ -41,17 +43,18 @@ function Team() {
       gitHub: "https://github.com/shaxzod67",
     },
     {
-        image:
-          "https://avatars.mds.yandex.net/i?id=04c92f2d7947f2f3460e996c8ee063609d02026a-5232865-images-thumbs&n=13",
-        name: "Yormuhammad",
-        surname: "G'aniyev",
-        position: "Direktor",
-        university: "Toshkent Axborot Texnologiyalar Universiteti",
-        experience: "4-yil",
-        telegram: "https://t.me/olimjonov_44",
-        instagram: "https://www.instagram.com/olimjonov.23",
-        gitHub: "https://github.com/shaxzod67",
-      },
+      id:3,
+      image:
+        "https://avatars.mds.yandex.net/i?id=04c92f2d7947f2f3460e996c8ee063609d02026a-5232865-images-thumbs&n=13",
+      name: "Yormuhammad",
+      surname: "G'aniyev",
+      position: "Direktor",
+      university: "Toshkent Axborot Texnologiyalar Universiteti",
+      experience: "4-yil",
+      telegram: "https://t.me/olimjonov_44",
+      instagram: "https://www.instagram.com/olimjonov.23",
+      gitHub: "https://github.com/shaxzod67",
+    },
   ];
   return (
     <div className="team">
@@ -70,11 +73,15 @@ function Team() {
                 </h3>
                 <p>{mall.university}</p>
                 <div className="team_link">
-                  <a href={mall.telegram}><FaTelegram />
+                  <a href={mall.telegram}>
+                    <FaTelegram />
                   </a>
-                  <a href={mall.instagram}><BiLogoInstagramAlt />
+                  <a href={mall.instagram}>
+                    <BiLogoInstagramAlt />
                   </a>
-                  <a href={mall.gitHub}><FaGithub /></a>
+                  <a href={mall.gitHub}>
+                    <FaGithub />
+                  </a>
                 </div>
               </div>
             </div>
